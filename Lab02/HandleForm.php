c90c2e2bee901a22d6086fa2df0511164c4f81ea
Hongbeubeu <44913755+Hongbeubeu@users.noreply.github.com>
2020-10-10 22:59:48 +0700

    fix handle other hobby

--- Lab02/HandleForm.php
+++ Lab02/HandleForm.php
@@ -3,13 +3,14 @@
     echo "You are studying at " . $_POST['class'] . ", ". $_POST['university']."<br/>";
     echo "Your hobby is". "<br/>";
     $hobbies = $_POST['hobby'];
+    $counter = 1;
     if(empty($hobbies)){
         echo "No hobby"."<br/>";
     }else{
-        $counter = 1;
         foreach($hobbies as $hobby){
-            echo $counter . "." . $hobby . "<br/>";
+            echo $counter . ". " . $hobby . "<br/>";
             $counter++;
         }
     }
+    echo $counter . ". " . $_POST['otherHobby']."<br/>";
 ?>
