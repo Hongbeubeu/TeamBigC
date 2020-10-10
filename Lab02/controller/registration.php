9e19529cab70b684eb72a84f04dee64a086c840a
superman2881999 <killteammate@gmail.com>
2020-10-10 23:57:31 +0700

    lab02

---
+++ Lab02/controller/registration.php
@@ -0,0 +1,16 @@
+<HTML>
+<HEAD><TITLE>Trang chủ</TITLE>
+<meta charset="utf-8" />
+<link rel="stylesheet" href="trangchu.css">
+</HEAD>
+<Body>
+<div class="alert success">
+  <strong>Xin chào!</strong>
+  <?php 
+    echo $_GET["fullname"];
+  ?> 
+</div>
+<div>
+</div>
+</body>
+</HTML>
