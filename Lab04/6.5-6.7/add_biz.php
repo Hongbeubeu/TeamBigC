bd7116e247f6aa95320a3f6ff0fb873e3e7ef859
superman2881999 <killteammate@gmail.com>
2020-11-02 17:52:04 +0700

    add 6.6

--- Lab04/6.5-6.7/add_biz.php
+++ Lab04/6.5-6.7/add_biz.php
@@ -1,13 +1,8 @@
 <html>
-    <head><title>Business Registration</title></head>
-    <style>
-    table, th, td {
-        border: 1px solid black;
-    }
-    .flex-container {
-        display: flex;
-    }
-</style>
+    <head>
+        <title>Business Registration</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
 <body>
  <?php
  require_once 'config.php';
@@ -23,10 +18,10 @@
         $result2 = mysqli_query( $connect, $SQLcmd2);
 ?>
  <div class="flex-container">
- <div>
+ <div >
     <p>Click on one, or control-click on multiple categories:</p>
     <form action="add_biz.php" method="post">
-    <select name="categoris" multiple>
+    <select name="categoris[]" multiple>
     <?php
        
         if ($result2->num_rows > 0) {
@@ -44,66 +39,63 @@
      die ("Select faild SQLcmd=$SQLcmd2");
      }
  ?>
- <div>
-        <label for="businessName">Business Name:</label>
-        <input type="text" name="businessName" /><br/>
-        <label for="address">Address:</label>
-        <input type="text" name="address" /><br/>
-        <label for="city">City:</label>
-        <input type="text" name="city" /><br/>
-        <label for="telephone">Telephone:</label>
-        <input type="text" name="telephone" /><br/>
-        <label for="url">URL:</l    abel>
-        <input type="text" name="url" /><br/>
-        
-           <button type="submit" name="submit">
-            <?php 
-            if(isset($_POST["submit"])){
-            ?>
-                <font>Add Another Business</font>
-            <?php
-            } else {
-            ?>
-               <font>Add Business</font>
-            <?php
-            }
-            ?>
-            </button>
-       
-    </form>
+ <div >
+        <p><label for="businessName">Business Name:</label>
+        <input type="text" name="businessName" /><br/></p>
+        <p><label for="address">Address:</label>
+        <input type="text" name="address" /><br/></p>
+        <p><label for="city">City:</label>
+        <input type="text" name="city" /><br/></p>
+        <p><label for="telephone">Telephone:</label>
+        <input type="text" name="telephone" /><br/></p>
+        <p><label for="url">URL:</label>
+        <input type="text" name="url" /><br/></p>
+   
 </div>
 </div>
-
+<button type="submit" name="submit">
+    <?php 
+    if(isset($_POST["submit"])){
+    ?>
+        <font>Add Another Business</font>
+    <?php
+    } else {
+    ?>
+        <font>Add Business</font>
+    <?php
+    }
+    ?>
+</button>
+</form>
 <?php
-
-if(empty($_POST['businessName'])||empty($_POST['address'])||empty($_POST['city'])||empty($_POST['telephone'])||empty($_POST['url'])){
-    echo "";
-}else{
-
-    $businessName = $_POST['businessName'];
-    $address = $_POST['address'];
-    $city = $_POST['city'];
-    $telephone = $_POST['telephone'];
-    $url = $_POST['url'];
-
-    $SQLcmd = "INSERT INTO $table_name (name,address,city,telephone,url) VALUES ('$businessName','$address','$city',$telephone,'$url')";
-    // $array = $result2->fetch_assoc();
-    // foreach($array as $value){
-    //     if($value == $array["category_id"]);
-    //     $SQLcmd3 = "INSERT INTO $table_name3 (category_id,business_id) VALUES ($value)";
-    // }
-
-    $SQLcmd3 = "INSERT INTO $table_name3 (category_id,business_id) VALUES ($_POST('categories')";
-    if (mysqli_query( $connect, $SQLcmd)){
-        
-        $result = mysqli_query( $connect, $SQLcmd);
-        //$roww = $result->fetch_assoc();
-
-        echo mysqli_insert_id($connect);
-    print "<br>The query is $SQLcmd";
-     } else {
-     echo "Error: " . $SQLcmd . "<br>" . mysqli_error($connect);
-     }
+if(isset($_POST["submit"])){
+    if(empty($_POST['businessName'])||empty($_POST['address'])||empty($_POST['city'])||empty($_POST['telephone'])||empty($_POST['url'])||empty($_POST['categoris'])){
+        echo "<br><font size='4' color='red'>You have not entered enough information</font>";
+    }else{
+    
+        $businessName = $_POST['businessName'];
+        $address = $_POST['address'];
+        $city = $_POST['city'];
+        $telephone = $_POST['telephone'];
+        $url = $_POST['url'];
+    
+        $SQLcmd = "INSERT INTO $table_name (name,address,city,telephone,url) VALUES ('$businessName','$address','$city',$telephone,'$url')";
+        if (mysqli_query( $connect, $SQLcmd)){
+    
+             $business_id = mysqli_insert_id($connect);
+    
+             foreach($_POST['categoris'] as $selectedOption){
+                $SQLcmd3 = "INSERT INTO $table_name3 (category_id,business_id) VALUES ($selectedOption,$business_id)";
+                if (mysqli_query( $connect, $SQLcmd)){
+                       echo "<br>Add " . $selectedOption . " business successed ";
+                } else {
+                echo "Error: " . $SQLcmd3 . "<br>" . mysqli_error($connect);
+                }
+            }
+         } else {
+         echo "Error: " . $SQLcmd . "<br>" . mysqli_error($connect);
+         }
+    }
 }
 ?>
 </body></html>
