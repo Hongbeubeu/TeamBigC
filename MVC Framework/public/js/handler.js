b0e38482ece9292a5091258c142ce1c34d7b18bc
Le Tuan <tuan.lm173442@sis.hust.edu.vn>
2020-12-14 23:24:11 +0700

    feat: create post form

--- MVC Framework/public/js/handler.js
+++ MVC Framework/public/js/handler.js
@@ -35,3 +35,31 @@ function showSlides(n, id) {
         slides[slideIndex-1].style.display = "block";
         dots[slideIndex-1].className += " active";
 }
+//modal
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var input = document.getElementById("newfeed__status");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+var buttonModal = document.getElementById("modal_button");
+console.log(buttonModal)
+// When the user clicks the button, open the modal 
+input.onclick = function() {
+  modal.style.display = "block";
+}
+buttonModal.onclick = function() {
+  modal.style.display = "none";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
