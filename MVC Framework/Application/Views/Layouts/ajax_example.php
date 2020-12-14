cc811f85b645bb9ed1727429d631edd16e1402e7
Hongbeubeu <44913755+Hongbeubeu@users.noreply.github.com>
2020-12-14 11:06:57 +0700

    test hint to next element of comment

--- MVC Framework/Application/Views/Layouts/ajax_example.php
+++ MVC Framework/Application/Views/Layouts/ajax_example.php
@@ -10,7 +10,11 @@ function showHint(str) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("txtHint").innerHTML = this.responseText;
+        var newElement = document.createElement('p');
+        console.log(this.responseText);
+        newElement.innerHTML = this.responseText;
+        var parent = document.getElementById('txtHint');
+        parent.appendChild(newElement);
       }
     }
     xmlhttp.open("GET", "/ajax-data/"+str, true);
@@ -22,10 +26,23 @@ function showHint(str) {
 <body>
 
 <p><b>Start typing a name in the input field below:</b></p>
-<form action="">
+
   <label for="fname">First name:</label>
-  <input type="text" id="fname" name="fname" onkeyup="showHint(this.value)">
-</form>
+  <input type="text" id="fname" name="fname" >
+  <button name="submit" onclick="showHint(document.getElementById('fname').value)"> Send </button>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br><br>
+<br>
+<br><br>
+<br>
+<br>
 <p>Suggestions: <span id="txtHint"></span></p>
 
 </body>
