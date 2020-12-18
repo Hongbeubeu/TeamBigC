b183f5b70b6f6d398273555ad8f3e283daba6853
Le Tuan <tuan.lm173442@sis.hust.edu.vn>
2020-12-18 22:27:23 +0700

    lremodule

--- MVC Framework/Application/Views/Feeds/newfeeds.php
+++ MVC Framework/Application/Views/Feeds/newfeeds.php
@@ -16,48 +16,10 @@
         <div class="newfeed__search">
             <input class="newfeed__search-input" placeholder="Search" type="search" />
         </div>
-        <div class="newfeed__control">
-            <input class="newfeed__input" id="newfeed__status" type="text" placeholder="What do you mind ?" />
-            <a href="#"><img src="/public/assets/add-image.png" class="icon-button icon_medium" /> </a>
-            <a href="#"><img src="/public/assets/stickers.png" class="icon-button icon_medium" /> </a>
-            <a href="#"><img src="/public/assets/video-call.png" class="icon-button icon_medium" /> </a>
-        </div>
+        <?php include (PATH_ROOT.DS.'Application'.DS.'Views'.DS.'Modules'.DS.'newfeed_control.php'); ?>
         <?php include (PATH_ROOT.DS.'Application'.DS.'Views'.DS.'Modules'.DS.'newfeed_main.php'); ?>
     </div>
-    <div class="suggestion">
-        <div class="suggestion__donate">
-            <p>SUGGESTION DONATE</p>
-            <div class="newfeed__identify identify" src="#">
-                <img class="avatar icon_medium" src="/public/assets/avatar.jpg" />
-                <p class="name" style="display: inline;">Hội hảo tâm giúp đỡ miền trung</p>
-            </div>
-            <div class="newfeed__identify identify" src="#">
-                <img class="avatar icon_medium" src="/public/assets/avatar.jpg" />
-                <p class="name" style="display: inline;">Hội hảo tâm giúp đỡ miền trung</p>
-            </div>
-            <div class="newfeed__identify identify" src="#">
-                <img class="avatar icon_medium" src="/public/assets/avatar.jpg" />
-                <p class="name" style="display: inline;">Hội hảo tâm giúp đỡ miền trung</p>
-            </div>
-
-        </div>
-        <div class="suggestion__add">
-            <p>SUGGESTION ADD</p>
-            <div class="newfeed__identify identify" src="#">
-                <img class="avatar icon_medium" src="/public/assets/avatar.jpg" />
-                <p class="name" style="display: inline;">Nguyễn Văn Hồng</p>
-            </div>
-            <div class="newfeed__identify identify" src="#">
-                <img class="avatar icon_medium" src="/public/assets/avatar.jpg" />
-                <p class="name" style="display: inline;">Nguyễn Văn Hồng</p>
-            </div>
-            <div class="newfeed__identify identify" src="#">
-                <img class="avatar icon_medium" src="/public/assets/avatar.jpg" />
-                <p class="name" style="display: inline;">Nguyễn Văn Hồng</p>
-            </div>
-        </div>
-
-    </div>
+    <?php include (PATH_ROOT.DS.'Application'.DS.'Views'.DS.'Modules'.DS.'suggestion.php'); ?>
     <?php include (PATH_ROOT.DS.'Application'.DS.'Views'.DS.'Modules'.DS.'new_post_modal.php'); ?>
 
 </body>
---
+++ MVC Framework/Application/Views/Modules/suggestion.php
@@ -0,0 +1,34 @@
+<div class="suggestion">
+        <div class="suggestion__donate">
+            <p>SUGGESTION DONATE</p>
+            <div class="newfeed__identify identify" src="#">
+                <img class="avatar icon_medium" src="/public/assets/avatar.jpg" />
+                <p class="name" style="display: inline;">Hội hảo tâm giúp đỡ miền trung</p>
+            </div>
+            <div class="newfeed__identify identify" src="#">
+                <img class="avatar icon_medium" src="/public/assets/avatar.jpg" />
+                <p class="name" style="display: inline;">Hội hảo tâm giúp đỡ miền trung</p>
+            </div>
+            <div class="newfeed__identify identify" src="#">
+                <img class="avatar icon_medium" src="/public/assets/avatar.jpg" />
+                <p class="name" style="display: inline;">Hội hảo tâm giúp đỡ miền trung</p>
+            </div>
+
+        </div>
+        <div class="suggestion__add">
+            <p>SUGGESTION ADD</p>
+            <div class="newfeed__identify identify" src="#">
+                <img class="avatar icon_medium" src="/public/assets/avatar.jpg" />
+                <p class="name" style="display: inline;">Nguyễn Văn Hồng</p>
+            </div>
+            <div class="newfeed__identify identify" src="#">
+                <img class="avatar icon_medium" src="/public/assets/avatar.jpg" />
+                <p class="name" style="display: inline;">Nguyễn Văn Hồng</p>
+            </div>
+            <div class="newfeed__identify identify" src="#">
+                <img class="avatar icon_medium" src="/public/assets/avatar.jpg" />
+                <p class="name" style="display: inline;">Nguyễn Văn Hồng</p>
+            </div>
+        </div>
+
+    </div>
